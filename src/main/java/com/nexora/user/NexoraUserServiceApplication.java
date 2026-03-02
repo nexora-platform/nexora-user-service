d9bb5dfda712697560a4c4e92e7729574d28c51e
Ali Al-Jalo <ali.aljalo@gmail.com>
2026-03-02 19:24:55 +0300

    chore: initialize user service project

---
+++ src/main/java/com/nexora/user/NexoraUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nexora.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexoraUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexoraUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexora/user/NexoraUserServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.nexora.user;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Disabled("Disabled until infrastructure configuration is ready")
+class NexoraUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
