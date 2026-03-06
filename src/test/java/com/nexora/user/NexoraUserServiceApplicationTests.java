270f5c856e3d630c1c03a6cfa3aef1fe1b4e47f8
Ali Al-Jalo <ali.aljalo@gmail.com>
2026-03-06 20:10:56 +0300

    test: add Testcontainers PostgreSQL for integration tests

--- src/test/java/com/nexora/user/NexoraUserServiceApplicationTests.java
+++ src/test/java/com/nexora/user/NexoraUserServiceApplicationTests.java
@@ -3,8 +3,10 @@ package com.nexora.user;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(TestPostgresContainer.class)
 @Disabled("Disabled until infrastructure configuration is ready")
 class NexoraUserServiceApplicationTests {
 
---
+++ src/test/java/com/nexora/user/TestPostgresContainer.java
@@ -0,0 +1,35 @@
+package com.nexora.user;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.testcontainers.containers.PostgreSQLContainer;
+import javax.sql.DataSource;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+
+@TestConfiguration
+public class TestPostgresContainer {
+
+    static final PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:15")
+            .withDatabaseName("testdb")
+            .withUsername("test")
+            .withPassword("test");
+
+    static {
+        postgresContainer.start();
+        System.setProperty("spring.datasource.url", postgresContainer.getJdbcUrl());
+        System.setProperty("spring.datasource.username", postgresContainer.getUsername());
+        System.setProperty("spring.datasource.password", postgresContainer.getPassword());
+    }
+
+    @Bean
+    @Primary
+    public DataSource dataSource() {
+        return DataSourceBuilder.create()
+                .url(postgresContainer.getJdbcUrl())
+                .username(postgresContainer.getUsername())
+                .password(postgresContainer.getPassword())
+                .driverClassName(postgresContainer.getDriverClassName())
+                .build();
+    }
+}
